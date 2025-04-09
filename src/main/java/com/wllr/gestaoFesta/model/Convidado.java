0fe864349520a1794b49d766de41b75e3457054d
Weller <weller_pereira@hotmail.com>
2025-04-09 00:57:00 -0300

    solucionado bug que prenevia a view de ser carregada com os objetos

--- src/main/java/com/wllr/gestaoFesta/model/Convidado.java
+++ src/main/java/com/wllr/gestaoFesta/model/Convidado.java
@@ -3,10 +3,12 @@ package com.wllr.gestaoFesta.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.io.Serializable;
 
 @Entity
+@Table(name= "convidados")
 public class Convidado implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -18,4 +20,28 @@ public class Convidado implements Serializable {
     private String nome;
 
     private Integer quantidadeAcompanhantes;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getQuantidadeAcompanhantes() {
+        return quantidadeAcompanhantes;
+    }
+
+    public void setQuantidadeAcompanhantes(Integer quantidadeAcompanhantes) {
+        this.quantidadeAcompanhantes = quantidadeAcompanhantes;
+    }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,3 @@
-insert into convidados (id, nome, quantidade_acompanhantes)
-    values (1, 'Pedro', 2);
-insert into convidados (id, nome, quantidade_acompanhantes)
-    values (2, 'Maria', 3);
-insert into convidados (id, nome, quantidade_acompanhantes)
-    values (3, 'Ricardo', 1);
+insert into convidados (id, nome, quantidade_acompanhantes) values (1, 'Pedro', 2);
+insert into convidados (id, nome, quantidade_acompanhantes) values (2, 'Maria', 3);
+insert into convidados (id, nome, quantidade_acompanhantes) values (3, 'Ricardo',   1);
