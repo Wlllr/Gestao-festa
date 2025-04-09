0514972b64340909b8f87a3e55113e0173d6d4e3
Weller <weller_pereira@hotmail.com>
2025-04-09 00:04:11 -0300

    enviando um objeto do controller para a view

--- src/main/java/com/wllr/gestaoFesta/controller/ConvidadosController.java
+++ src/main/java/com/wllr/gestaoFesta/controller/ConvidadosController.java
@@ -1,13 +1,23 @@
 package com.wllr.gestaoFesta.controller;
 
+import com.wllr.gestaoFesta.repository.Convidados;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ConvidadosController {
 
+    @Autowired
+    private Convidados convidados;
+
     @GetMapping("/convidados")
-    public String listar() {
-        return "ListaConvidados";
+    public ModelAndView listar() {
+        ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+
+        modelAndView.addObject("convidados", convidados.findAll());
+
+        return modelAndView;
     }
 }
