0c926bad5869171c28fb487c71d9dbc63407a4b3
Weller <weller_pereira@hotmail.com>
2025-04-09 00:22:25 -0300

    listando os objetos na view com o Thymeleaf

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into convidados (id, nome, quantidade_acompanhantes)
+    values (1, 'Pedro', 2);
+insert into convidados (id, nome, quantidade_acompanhantes)
+    values (2, 'Maria', 3);
+insert into convidados (id, nome, quantidade_acompanhantes)
+    values (3, 'Ricardo', 1);
