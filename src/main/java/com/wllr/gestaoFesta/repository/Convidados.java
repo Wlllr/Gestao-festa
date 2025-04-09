f4b2d668fca565dda325977fb416a61e739d3c98
Weller <weller_pereira@hotmail.com>
2025-04-08 23:51:58 -0300

    interface que representará um repositório de convidados

---
+++ src/main/java/com/wllr/gestaoFesta/repository/Convidados.java
@@ -0,0 +1,8 @@
+package com.wllr.gestaoFesta.repository;
+
+import com.wllr.gestaoFesta.model.Convidado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Convidados extends JpaRepository<Convidado, Long> {
+
+}
